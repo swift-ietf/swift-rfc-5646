a60b80cd70b78fca9399479715fc334abe53b6d3
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 15:04:05 +0200

    Converge dependency locations to their canonical organizations

--- Package.swift
+++ Package.swift
@@ -1,33 +1,6 @@
 // swift-tools-version: 6.4
 import PackageDescription
 
-extension String {
-    static let rfc5646: Self = "RFC 5646"
-    static let rfc5646Tests: Self = rfc5646 + " Tests"
-
-    static let iso639: Self = "ISO 639"
-    static let iso3166: Self = "ISO 3166"
-    static let iso15924: Self = "ISO 15924"
-
-    var tests: Self { self + " Tests" }
-}
-
-extension PackageDescription.Target.Dependency {
-    static let iso639: Self = .product(name: "ISO 639", package: "swift-iso-639")
-    static let iso3166: Self = .product(name: "ISO 3166", package: "swift-iso-3166")
-    static let iso15924: Self = .product(name: "ISO 15924", package: "swift-iso-15924")
-    static let standards: Self = .product(
-        name: "Standard Library Extensions",
-        package: "swift-standard-library-extensions"
-    )
-    static let ascii: Self = .product(
-        name: "ASCII",
-        package: "swift-ascii"
-    )
-
-    static let rfc5646: Self = .target(name: .rfc5646)
-}
-
 let package = Package(
     name: "swift-rfc-5646",
     platforms: [
@@ -60,18 +33,18 @@ let package = Package(
         .target(
             name: "RFC 5646",
             dependencies: [
-                .standards,
-                .ascii,
-                .iso639,
-                .iso3166,
-                .iso15924,
+                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
+                .product(name: "ASCII", package: "swift-ascii"),
+                .target(name: "ISO 639"),
+                .target(name: "ISO 3166"),
+                .target(name: "ISO 15924"),
                 .product(name: "Parser", package: "swift-parser"),
             ]
         ),
         .testTarget(
             name: "RFC 5646 Tests",
             dependencies: [
-                "RFC 5646"
+                .target(name: "RFC 5646")
             ]
         ),
     ],
